ac664b2368ed9ff1b80a84a35103683bba73ae80
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-11-03 19:49:01 +0000

    vdhj

--- classes/AiPlayer.js
+++ classes/AiPlayer.js
@@ -6,12 +6,19 @@ class AiPlayer extends Player {
         super(id, randomElement(["PlopyFun", "RoadCode", "32hropat", "Typer32", "McGod", "MitBlade", "Killer", "12345", "Hacker326", "sword", "swordgod", "sword.io", "player", "Alex", "Rajesh", "Ram","Emily", "Steve", "Max", "Lily", "Rohit", "Shiva", "Krishna", "Fan"]))
         this.ai = true;
         this.target = undefined;
+        this.lastHit = Date.now()
     }
     tick(players, coins, io) {
-
+const lerp = (x, y, a) => x * (1 - a) + y * a; 
       if(!this.target || !this.entityExists(this.target, this.getEntities(players, coins))) this.target = this.getClosestEntity(this.getEntities(players, coins))
       if(this.target) {
-        if(this.target.type==="player") console.log(this.target.name)
+        if(this.target.type==="player" && Date.now() - this.lastHit < 100) {
+         var f = this.down(!this.mouseDown, players, coins, io)
+         players = f[0]
+         coins = f[1]
+        } 
+        this.mousePos.x = this.mousePos.viewport.width / 2 + (this.target.pos.x - this.pos.x)
+        this.mousePos.y = this.mousePos.viewport.height / 2 + (this.target.pos.y - this.pos.y)
         var controller = this.getController()
         players[this.id] = this.move(controller, players)
         var f = this.collectCoins(players, coins, io)
