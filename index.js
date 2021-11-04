907f6d47956ec883db7dfed59b75f7f7f4af1c24
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-11-04 19:40:45 +0000

    k

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ setInterval(async () => {
   if (Object.values(PlayerList.players).filter(p => p && p.ai).length < maxAiPlayers) {
     var id = uuidv4()
     var theAi = new AiPlayer(id)
-    console.log("AI Player Joined -> "+id)
+    console.log("AI Player Joined -> "+theAi.name)
     PlayerList.setPlayer(id, theAi)
     io.sockets.emit('new', theAi)
   }
