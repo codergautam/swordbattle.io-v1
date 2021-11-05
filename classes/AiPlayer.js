98ad7251c8d78977947c754cceda8ee6f3ff9d39
Coder Gautam <gautamgxtv@gmail.com>
2021-11-05 09:34:14 -0500

    more names nice

--- classes/AiPlayer.js
+++ classes/AiPlayer.js
@@ -6,7 +6,7 @@ function getRandomInt(min, max) {
 class AiPlayer extends Player {
     constructor(id) {
         const randomElement = (array) => array[Math.floor(Math.random() * array.length)];
-        super(id, randomElement(["PlopyFun", "RoadCode", "32hropat", "Typer32", "McGod", "MitBlade", "Killer", "12345", "Hacker326", "sword", "swordgod", "sword.io", "player", "Alex", "Rajesh", "Ram","Emily", "Steve", "Max", "Lily", "Rohit", "Shiva", "Krishna", "Fan", "Liam", "Noah", "Emma", "Olivia"]))
+        super(id, randomElement(["PlopyFun", "RoadCode", "32hropat", "Typer32", "McGod", "MitBlade", "Killer", "12345", "Hacker326", "sword", "swordgod", "sword.io", "player", "Alex", "Rajesh", "Ram","Emily", "Steve", "Max", "Lily", "Rohit", "Shiva", "Krishna", "Fan", "Liam", "Noah", "Emma", "Olivia", "mollthecoder", "RayhanADev", "amasad", "Elon Musk", "Jeff Bezos", "ur bad LMAO", "killsword", "swordKILLER", "EvasiveCollecter", "123asd", "Name", "Yeet", "Idiot", "Tamil", "Hindi", "Telugu", "Kannada", "Malayalam", "USA", "Pakistan", "Malaysia", "UAE", "Enter name"]))
         this.ai = true;
         this.target = undefined;
         this.lastHit = Date.now()
