bb8a6c414eb155d9b4b63040c0da045d65c7baef
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-11-08 18:34:51 +0000

    m

--- src/TitleScene.js
+++ src/TitleScene.js
@@ -101,7 +101,7 @@ this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
   });
   this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
   this.returnKey.on("down", event => {
-   if(this.promo) {
+   if(this.promo && this.promo.visible) {
      this.promo.destroy()
    } else go()
   });
