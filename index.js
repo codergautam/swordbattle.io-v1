829e36f2f8db011dee925ec574217bbb270e6a26
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-11-09 23:38:30 +0000

    repl

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var production = true;
 if (production) {
   const rateLimit = require('express-rate-limit');
   const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 100, // limit each IP to 100 requests per windowMs
   });
   app.use('/', limiter);
