08e8d64bb229075c0e55c89056492419a399893a
Coder Gautam <gautamgxtv@gmail.com>
2021-11-13 21:06:39 -0600

    balance

--- classes/Player.js
+++ classes/Player.js
@@ -123,9 +123,8 @@ this.pos.y = pos[1]
         touching.forEach((coin) => {
           this.coins += 1;
           if (this.scale > 7.5) var increase = 0.00005;
-          else if (this.scale > 5) var increase = 0.00001;
-          else if (this.scale > 2.5) var increase = 0.000016;
-          else var increase = 0.0001;
+          else if (this.scale > 2.5) var increase = 0.0001;
+          else var increase = 0.0002;
           this.scale += increase;
           var index = coins.findIndex((e) => e.id == coin.id);
           coins.splice(index, 1);
