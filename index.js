4900b7591e44ba9ee66b8a5f9e66c63967808dae
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2022-03-01 00:11:15 +0000

    no more cors

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var http = require("http");
 require("dotenv").config();
 const { Server } = require("socket.io");
 const app = express();
-var cors = require("cors");
 var emailValidator = require("email-validator");
 const bcrypt = require("bcrypt");
 var uuid = require("uuid");
 var fs = require("fs");
+var cors = require('cors')
 
 var server;
 /*
@@ -133,7 +133,24 @@ oldlevels.forEach((level, index)  =>{
 
 moderation.start(app);
 
-//app.use(cors());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use("/", express.static("dist"));
