8323fe76a84d7cf6be13bc2b50aacd922ab9e7b1
Coder Gautam <gautamgxtv@gmail.com>
2022-03-25 14:36:28 -0500

    e

--- src/GameScene.js
+++ src/GameScene.js
@@ -585,7 +585,7 @@ class GameScene extends Phaser.Scene {
                     this.lvlBar.setLerpValue((lvlcoins / diff)*100);
 
 					this.lvlState.setText("Level: " + player.level +" ("+Math.round((lvlcoins/diff)*100)+"%)");
-					if(player.level == this.levels.length) {
+					if(player.level > this.levels.length+1) {
 						this.lvlState.setText("Max Level");
 					}
 					if(this.myObj && player.level > this.myObj.level) {
