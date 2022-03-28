15557683224d1a479dd46cef92321354b13eab6d
Coder Gautam <gautamgxtv@gmail.com>
2022-03-28 18:16:34 -0500

    e

--- src/OpenScene.js
+++ src/OpenScene.js
@@ -185,7 +185,7 @@ class OpenScene extends Phaser.Scene {
         this.euText.y += ((this.euRect.height/2) - (this.euText.height/2));
         this.usText.y += ((this.usRect.height/2) - (this.usText.height/2));
         var euUrl = "swordbattle.herokuapp.com";
-        var naUrl = "swordbattle.io";
+        var naUrl = "www.swordbattle.io";
         var time = Date.now();
         this.data = {};
        
