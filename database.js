f830828165e6f86638c148925042ec7827053f42
Coder Gautam <gautamgxtv@gmail.com>
2022-05-16 18:21:39 -0500

    Db change

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const postgres = require("postgres");
 
-const sql = postgres(process.env.DATABASE_URL, {ssl: true});
+const sql = postgres(process.env.DATABASE_URL, {ssl: {rejectUnauthorized:false}});
 
 module.exports = {sql: sql};
