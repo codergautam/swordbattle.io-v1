f76640a9f78bdffdc84083b31df548b4805b75fa
Coder Gautam <gautamgxtv@gmail.com>
2022-05-22 06:17:51 -0500

    E

--- index.js
+++ index.js
@@ -9,72 +9,9 @@ const bcrypt = require("bcrypt");
 var uuid = require("uuid");
 var fs = require("fs");
 var process = require("process");
-const { exec } = require("child_process");
 
 
-var testenv =
-`TOKEN=test
-CAPTCHASECRET=6LeIewsgAAAAABWjEVCnFPR7POHFJbzZJM_OqKdQ
-CAPTCHASITE=6LeIewsgAAAAAPp9VS21fBk7VWQX3wps40gWrUWH
 
-USEFISHYSSL=false
-DATABASE_URL=postgres://fishymine:P7oU3-qbHjgPTFnZRat7kw@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/bilked-blob-1224.defaultdb
-PRODUCTION=false`;
-
-var defaultconfig = `{
-  "CAPTCHASITE": "6LeIewsgAAAAAPp9VS21fBk7VWQX3wps40gWrUWH",
-  "localServer": true
-}`;
-
-//check if config.json exists
-if (!fs.existsSync("./config.json")) {
-	console.log("Generating config.json");
-	fs.writeFileSync("./config.json", "{}");
-}
-var theConfig = require("./config.json");
-
-var serverState = "running";
-
-var map = 10000;
-//var cors = require("cors");
-
-var server;
-var httpsserver;
-
-
-if(!theConfig.hasOwnProperty("localServer")) {
-	console.log("Generating config.json");
-	fs.writeFileSync("./config.json", defaultconfig);
-	theConfig = require("./config.json");
-	exec("npm run build");
-}
-
-if(!process.env.TOKEN) {
-	console.log("👀 First run? No worries, we're setting you up.");
-	fs.writeFileSync(".env", testenv);
-	fs.writeFileSync("config.json", defaultconfig);
-	console.log("Building latest code... ");
-	theConfig = require("./config.json");
-	//run npm run build
-	exec("npm run build");
-	console.log("✅ Done! Please re-run this script to start a server!\n\nMade with <3 by gautam.");
-	process.exit(1);
-}
-
-if(process.env.PRODUCTION == "true") {
-	console.log("🚀 Using production setup...\n");
-	if(process.env.CAPTCHASITE) {
-		theConfig.CAPTCHASITE = process.env.CAPTCHASITE;
-	}
-	theConfig.localServer = false;
-	fs.writeFileSync("config.json", JSON.stringify(theConfig));
-	console.log("Note: We've updated the config.json file to reflect this.\n");
-	console.log("If you're running this during development, the game won't work\n");
-	console.log("🔥 Creating a production build...");
-	exec("npm run build");
-	console.log("✅ Done! You can make a PR with this code and it will be suitable for production\n");
-
-}
 //console.log(fs.readFileSync("/etc/letsencrypt/live/test.swordbattle.io/fullchain.pem"))
 var usinghttps = false;
 if (process.env.USEFISHYSSL === "true") {
