1a60534dda9e2c92b58891598b18c80d4f4ea58a
Coder Gautam <gautamgxtv@gmail.com>
2022-06-02 06:43:57 -0500

    clean

--- classes/evolutions/index.js
+++ classes/evolutions/index.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 var evos = {};
 fs.readdirSync("./classes/evolutions").forEach((file) => {
   if(file == "index.js" || file == "Evolution.js") return;
-  var evo = require("./"+file)
-  evo = new evo()
+  var evo = require("./"+file);
+  evo = new evo();
   evos[evo.name] = evo;
-})
-console.log("Loaded "+Object.keys(evos).length+" evolutions")
-module.exports = evos
+});
+console.log("Loaded "+Object.keys(evos).length+" evolutions");
+module.exports = evos;
