48f141d0d16f85e2b8765c580d5cfc663ebbcbb0
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2022-10-06 17:21:13 +0000

    Chest fix

--- classes/Chest.js
+++ classes/Chest.js
@@ -14,32 +14,32 @@ function getRandomInt(min, max) {
       this.raritys = {
         normal: {
           scale: 0.5,
-          drop: [20,100],
+          drop: [100,250],
           health: 1
         },
         uncommon: {
           scale: 0.65,
-          drop: [50,200],
+          drop: [250,500],
           health: 25
         },
         rare: {
           scale: 0.75,
-          drop: [150,300],
+          drop: [500,1000],
           health: 50,
         },
         epic: {
           scale: 1,
-          drop: [200,500],
+          drop: [1000,2000],
           health: 100
         },
         legendary: {
           scale: 1.5,
-          drop: [500,1000],
+          drop: [2000,5000],
           health: 250
         },
         mythical: {
           scale: 2.25,
-          drop: [1000,5000],
+          drop: [5000,10000],
           health: 500
         }
       };
@@ -63,11 +63,31 @@ function getRandomInt(min, max) {
     }
     open() {
       var drop = [];
-      for (var i = 0; i < getRandomInt(this.drop[0],this.drop[1]); i++) {
+      var toDrop = getRandomInt(this.drop[0],this.drop[1]);
+      var coinSizes = [5,4,3, 2, 1];
+      if(toDrop > 500) coinSizes.unshift(15)
+      if(toDrop > 1000) coinSizes.unshift(25)
+      
+      
+      // for (var i = 0; i < getRandomInt(this.drop[0],this.drop[1]); i++) {
+      //   var x = getRandomInt(this.pos.x, this.pos.x + this.width);
+      //   var y = getRandomInt(this.pos.y, this.pos.y + this.height);
+
+      //   drop.push(new Coin({ x: x, y: y }));
+      // }
+      while (toDrop > 0) {
+        // Find biggest coinsize that fits in toDrop
+        var usedCoinSize = 0;
+        for(var coinSize of coinSizes) {
+          if(toDrop >= coinSize) {
+            usedCoinSize = coinSize;
+            break;
+          }
+        }
         var x = getRandomInt(this.pos.x, this.pos.x + this.width);
         var y = getRandomInt(this.pos.y, this.pos.y + this.height);
-
-        drop.push(new Coin({ x: x, y: y }));
+        drop.push(new Coin({ x,y, value: usedCoinSize}));
+        toDrop -= usedCoinSize;
       }
       return drop;
     }
