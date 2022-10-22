f1047a17e688898cf013d662435a0bee7c58c31e
Coder Gautam <gautamgxtv@gmail.com>
2022-10-22 07:43:20 -0500

    Module styles

--- src/client/components/game/ui/TitleUI.tsx
+++ src/client/components/game/ui/TitleUI.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../../../css/title.module.css";
 
 export default function TitleUI() {
 return (
   <div>
-    <h1>Swordbattle.io</h1>
+    <h1 >Swordbattle.io</h1>
   </div>
 )
 }
