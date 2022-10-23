9d45602e2eaeb36a620408c5a4883892dc4c1481
Coder Gautam <gautamgxtv@gmail.com>
2022-10-23 10:19:34 -0500

    Styling+elems

--- src/client/components/game/ui/TitleUI.tsx
+++ src/client/components/game/ui/TitleUI.tsx
@@ -4,9 +4,10 @@ import styles from "../../../css/title.module.css";
 export default function TitleUI() {
 return (
   <div className={styles.background}>
-    <h1>Swordbattle.io</h1>
-    <button>Play</button>
-    
+    <h1 className={styles.titletext}>Swordbattle.io</h1>
+    <input type="text" className={styles.namebox} placeholder="Name" />
+    <button className={styles.playbtn}>Play</button>
+
   </div>
 )
 }
