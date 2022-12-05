86349ca239d6a6f2315f0ae6649327fea262a5ce
robigan <35210888+robigan@users.noreply.github.com>
2022-12-05 20:03:19 +0000

    Add support for IPv4 loopback address

--- src/client/components/game/helpers/getServerUrl.ts
+++ src/client/components/game/helpers/getServerUrl.ts
@@ -1,8 +1,8 @@
 export default function getServerUrl() {
   // if localhost, use localhost
-  if (window.location.hostname === 'localhost') {
+  if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') { // IPv6 in browsers isn't properly supported yet, but IPv4 is. So add IPv4 loopback hostname detection
     const currentPort = window.location.port;
-    return `ws://localhost:${currentPort}`;
+    return `ws://${window.location.hostname}:${currentPort}`;
   }
   // if not localhost, use the current hostname
   return `wss://${window.location.hostname}`;
