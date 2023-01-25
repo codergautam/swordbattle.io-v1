fc7fbe423606eb6c7177d9425b98b5aa4012ccf0
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2023-01-25 10:26:25 -0600

    Increase ad delay

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ document.body.style.webkitTransform =       // Chrome, Opera, Safari
  document.body.style.msTransform =          // IE 9
  document.body.style.transform = scale;     // General
 
-var adDelay = 300000;
+var adDelay = 450000;
 var gameScene = new GameScene((data) => {
     titleScene.playPreroll = (playPreroll && Date.now() - lastAd > adDelay);
 });
