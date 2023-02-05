13826307e3a1301c622ebaeafb69291a5978ea6b
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2023-02-05 17:27:00 +0000

    contentType headers

--- src/server/http/index.ts
+++ src/server/http/index.ts
@@ -1,15 +1,45 @@
 import path from 'path';
 import fs from 'fs';
 
+function contentType(p: string, res: { writeHeader: (arg0: string, arg1: string) => void; }){
+  const ext = path.extname(p);
+  switch (ext) {
+    case '.html':
+      res.writeHeader('Content-Type', 'text/html');
+      break;
+    case '.js':
+      res.writeHeader('Content-Type', 'text/javascript');
+      break;
+    case '.css':
+      res.writeHeader('Content-Type', 'text/css');
+      break;
+    case '.json':
+      res.writeHeader('Content-Type', 'application/json');
+      break;
+    case '.png':
+      res.writeHeader('Content-Type', 'image/png');
+      break;
+    case '.jpg':
+      res.writeHeader('Content-Type', 'image/jpg');
+      break;
+    case '.wav':
+      res.writeHeader('Content-Type', 'audio/wav');
+      break;
+  }
+}
+
+
 export default (res: any, req: any) => {
   try {
     const url = req.getUrl();
     const p = `../../../dist${url === '/' ? '/index.html' : url}`;
+    contentType(p, res);
     res.end(fs.readFileSync(path.resolve(__dirname, p)));
   } catch (e) {
     try {
       const url = req.getUrl();
       const p = `../../../public${url}`;
+      contentType(p, res);
       res.end(fs.readFileSync(path.resolve(__dirname, p)));
     } catch {
       res.writeStatus('404');
