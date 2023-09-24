8243becdefd16237e80a7afd878d9ad5ff601668
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2023-09-24 16:51:12 -0500

    Update Archergod.js

--- classes/evolutions/Archergod.js
+++ classes/evolutions/Archergod.js
@@ -22,7 +22,7 @@ class Archergod extends Evolution {
             healAmount: 1.8,
             healWait: 2,
             throwCooldown: 0.5,
-            throwDamageMultiplier: 110,
+            throwDamageMultiplier: 7,
         };
     }
     ability() {
